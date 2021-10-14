82ed35471a42cf3df2ce88e06efce8c18e82ac4f
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-14 22:57:57 +0200

    Actualización vacunacion.java

--- GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/Vacunacion.java
+++ GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/Vacunacion.java
@@ -11,5 +11,62 @@ public class Vacunacion {
 	VacunacionDAO vacunacionDao;
 	private Date fecha;
 	private boolean isSegundaDosis;
+	
+	public Vacunacion(TipoVacuna vacuna, Paciente paciente, VacunacionDAO vacunacionDao, Date fecha,
+			boolean isSegundaDosis) {
+		super();
+		this.vacuna = vacuna;
+		this.paciente = paciente;
+		this.vacunacionDao = vacunacionDao;
+		this.fecha = fecha;
+		this.isSegundaDosis = isSegundaDosis;
+	}
+
+	public TipoVacuna getVacuna() {
+		return vacuna;
+	}
+
+	public void setVacuna(TipoVacuna vacuna) {
+		this.vacuna = vacuna;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public VacunacionDAO getVacunacionDao() {
+		return vacunacionDao;
+	}
+
+	public void setVacunacionDao(VacunacionDAO vacunacionDao) {
+		this.vacunacionDao = vacunacionDao;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public boolean isSegundaDosis() {
+		return isSegundaDosis;
+	}
+
+	public void setSegundaDosis(boolean isSegundaDosis) {
+		this.isSegundaDosis = isSegundaDosis;
+	}
+
+	@Override
+	public String toString() {
+		return "Vacunacion [vacuna=" + vacuna + ", paciente=" + paciente + ", vacunacionDao=" + vacunacionDao
+				+ ", fecha=" + fecha + ", isSegundaDosis=" + isSegundaDosis + "]";
+	}
+	
 
 }
