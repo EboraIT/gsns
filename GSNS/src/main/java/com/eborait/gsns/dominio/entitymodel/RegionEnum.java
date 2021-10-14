1898b312023654ff4860696665e730eabed63dd7
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-14 22:59:34 +0200

    Actualizacion RegionEnum

--- GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/RegionEnum.java
+++ GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/RegionEnum.java
@@ -2,9 +2,27 @@ package com.eborait.gsns.dominio.entitymodel;
 
 import java.util.*;
 
-public enum RegionEnum {
-	;
+public class RegionEnum{
 
 	Collection<EntregaVacunas> entregas;
+
+	public RegionEnum(Collection<EntregaVacunas> entregas) {
+		super();
+		this.entregas = entregas;
+	}
+
+	public Collection<EntregaVacunas> getEntregas() {
+		return entregas;
+	}
+
+	public void setEntregas(Collection<EntregaVacunas> entregas) {
+		this.entregas = entregas;
+	}
+
+	@Override
+	public String toString() {
+		return "RegionEnum [entregas=" + entregas + "]";
+	}
+	
 
 }
