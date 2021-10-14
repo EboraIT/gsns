dc3c59536d69e1ea62041b8a1d308176da1b666a
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-14 22:50:06 +0200

    Se añaden getter y setter

--- GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/TipoVacuna.java
+++ GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/TipoVacuna.java
@@ -5,5 +5,35 @@ public class TipoVacuna {
 	private String nombre;
 	private String farmaceutica;
 	private String fechaAprobacion;
+	
+	public TipoVacuna(String nombre, String farmaceutica, String fechaAprobacion) {
+		super();
+		this.nombre = nombre;
+		this.farmaceutica = farmaceutica;
+		this.fechaAprobacion = fechaAprobacion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getFarmaceutica() {
+		return farmaceutica;
+	}
+	public void setFarmaceutica(String farmaceutica) {
+		this.farmaceutica = farmaceutica;
+	}
+	public String getFechaAprobacion() {
+		return fechaAprobacion;
+	}
+	public void setFechaAprobacion(String fechaAprobacion) {
+		this.fechaAprobacion = fechaAprobacion;
+	}
+	@Override
+	public String toString() {
+		return "TipoVacuna [nombre=" + nombre + ", farmaceutica=" + farmaceutica + ", fechaAprobacion="
+				+ fechaAprobacion + "]";
+	}
 
 }
