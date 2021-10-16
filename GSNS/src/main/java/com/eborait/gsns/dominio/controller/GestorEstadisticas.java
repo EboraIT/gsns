5fc971c8ad3e08faae95dfbef662230df2c194b7
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-16 11:06:39 +0200

    Actualización de metodos consultarTotalVacunados nacional y por region

--- GSNS/src/main/java/com/eborait/gsns/dominio/controller/GestorEstadisticas.java
+++ GSNS/src/main/java/com/eborait/gsns/dominio/controller/GestorEstadisticas.java
@@ -1,21 +1,35 @@
 package com.eborait.gsns.dominio.controller;
 
 import com.eborait.gsns.dominio.entitymodel.*;
+import com.eborait.gsns.persistencia.VacunacionDAO;
 
 public class GestorEstadisticas {
 
-	public void consultarTotalVacunados() {
-		// TODO - implement GestorEstadisticas.consultarTotalVacunados
-		throw new UnsupportedOperationException();
+	public int consultarTotalVacunados() throws Exception {
+		int totalVacunados=0;
+		VacunacionDAO v = null ;
+		
+		for (int i=0;i<v.seleccionarVacunaciones().size();i++) {
+			totalVacunados=+1;
+		}
+		return totalVacunados;
+	
+		
 	}
 
 	/**
 	 * 
 	 * @param region
+	 * @return 
 	 */
-	public void consultarTotalVacunadosPorRegion(RegionEnum region) {
-		// TODO - implement GestorEstadisticas.consultarTotalVacunadosPorRegion
-		throw new UnsupportedOperationException();
+	public int consultarTotalVacunadosPorRegion(RegionEnum region) {
+		int totalVacunadosRegion=0;
+		VacunacionDAO v = null ;
+		
+		for (int i=0;i<v.seleccionarVacunaciones(region).size();i++) {
+			totalVacunadosRegion=+1;
+		}
+		return totalVacunadosRegion;
 	}
 
 	public void consultarPorcentajeVacunadosSobreRecibidas() {
