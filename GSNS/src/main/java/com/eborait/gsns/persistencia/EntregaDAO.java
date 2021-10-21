29adacd54a4316a3d12941b6a9c68da831a2a9e0
escolanojorge <82335141+escolanojorge@users.noreply.github.com>
2021-10-22 01:35:07 +0200

    Se añade la operación insert a EntregaDAO.

--- GSNS/src/main/java/com/eborait/gsns/persistencia/EntregaDAO.java
+++ GSNS/src/main/java/com/eborait/gsns/persistencia/EntregaDAO.java
@@ -1,29 +1,49 @@
 package com.eborait.gsns.persistencia;
 
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import com.eborait.gsns.dominio.entitymodel.EntregaVacunas;
 
-import com.eborait.gsns.dominio.entitymodel.*;
+public class EntregaDAO extends AbstractEntityDAO<EntregaVacunas> {
+	private static final String INSERT = "INSERT INTO entrega_vacunas VALUES(%s, %s, %s, %s, %s, %s, %s)";
 
-public class EntregaDAO extends AbstractEntityDAO {
-
-	/**
-	 * 
-	 * @param entrega
-	 */
-	public void insertarEntrega(EntregaVacunas entrega) {
-		// TODO - implement EntregaDAO.insertarEntrega
-		throw new UnsupportedOperationException();
+	@Override
+	public EntregaVacunas get(String id) {
+		return null;
 	}
 
-	/**
-	 * 
-	 * @param region
-	 */
-	public List<EntregaVacunas> seleccionarEntregas(RegionEnum region) {
-		// TODO - implement EntregaDAO.seleccionarEntregas
-		throw new UnsupportedOperationException();
+	@Override
+	public Collection<EntregaVacunas> getAll(String criteria, String value) {
+		return null;
+	}
+
+	@Override
+	public int insert(EntregaVacunas entregaVacunas) throws SQLException {
+		try {
+			// TODO cambiar TODO por el nombre del método del tipovacuna de sus atributos
+			// combinados
+			return AgenteBD.getAgente()
+					.insert(String.format(INSERT, entregaVacunas.getId(), entregaVacunas.getFecha(),
+							entregaVacunas.getCantidad(), entregaVacunas.getLote().getId(),
+							entregaVacunas.getGrupoPrioridad().getPrioridad(), entregaVacunas.getTipo().TODO(),
+							entregaVacunas.getRegion().getId()));
+		} catch (SQLException sqle) {
+			System.out.println("Excepción insertando entrega:\n\n" + sqle.getStackTrace());
+			throw sqle;
+		}
+	}
+
+	@Override
+	public int update(EntregaVacunas entregaVacunas) {
+		return -1;
+	}
+
+	@Override
+	public int delete(EntregaVacunas entregaVacunas) {
+		return -1;
 	}
 
 }
