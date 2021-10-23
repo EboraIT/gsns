1b29123fb04e990033f2d96772c7d6b0b7cefa0f
escolanojorge <82335141+escolanojorge@users.noreply.github.com>
2021-10-23 03:10:48 +0200

    Se crean los métodos de conexión y desconexión en AgenteBD.

--- GSNS/src/main/java/com/eborait/gsns/persistencia/AgenteBD.java
+++ GSNS/src/main/java/com/eborait/gsns/persistencia/AgenteBD.java
@@ -1,12 +1,37 @@
 package com.eborait.gsns.persistencia;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AgenteBD {
+	/**
+	 * Instancia del agente.
+	 */
+	private static AgenteBD agente = null;
+	/**
+	 * Conexión con la base de datos.
+	 */
+	private static Connection conexion;
+	/**
+	 * Identificador ODBC de la base de datos.
+	 */
+	private static final String URL = BDConstantes.DRIVER + ":" + BDConstantes.DBNAME + ";create=false";
 
-	public void conectarBD() {
-		// TODO - implement AgenteBD.conectarBD
-		throw new UnsupportedOperationException();
+	public AgenteBD() throws SQLException {
+		conectarBD();
+	}
+
+	public static AgenteBD getAgente() throws SQLException {
+		return agente == null ? new AgenteBD() : agente;
+	}
+
+	public void conectarBD() throws SQLException {
+		Driver derbyEmbeddedDriver = new EmbeddedDriver();
+		DriverManager.registerDriver(derbyEmbeddedDriver);
+		conexion = DriverManager.getConnection(URL, BDConstantes.DBUSER, BDConstantes.DBPASS);
 	}
 
 	public void desconectarBD() {
@@ -17,6 +42,7 @@ public class AgenteBD {
 	/**
 	 * 
 	 * @param sql
+	 * @return
 	 */
 	public ResultSet select(String sql) {
 		// TODO - implement AgenteBD.select
@@ -26,6 +52,7 @@ public class AgenteBD {
 	/**
 	 * 
 	 * @param sql
+	 * @return
 	 */
 	public int insert(String sql) {
 		// TODO - implement AgenteBD.insert
@@ -35,6 +62,7 @@ public class AgenteBD {
 	/**
 	 * 
 	 * @param sql
+	 * @return
 	 */
 	public int update(String sql) {
 		// TODO - implement AgenteBD.update
@@ -44,15 +72,11 @@ public class AgenteBD {
 	/**
 	 * 
 	 * @param sql
+	 * @return
 	 */
 	public int delete(String sql) {
 		// TODO - implement AgenteBD.delete
 		throw new UnsupportedOperationException();
-	}
-
-	public static LoteVacunasDAO getAgente() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
