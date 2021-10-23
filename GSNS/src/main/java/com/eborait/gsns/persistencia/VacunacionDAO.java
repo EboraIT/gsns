257abb78b8f7f925311b8183795edc41bb1f35b2
escolanojorge <82335141+escolanojorge@users.noreply.github.com>
2021-10-23 15:53:41 +0200

    VacunacionDAO: Se implementa método insert con su sentencia sql.
    Se documenta la clase parcialmente.

--- GSNS/src/main/java/com/eborait/gsns/persistencia/VacunacionDAO.java
+++ GSNS/src/main/java/com/eborait/gsns/persistencia/VacunacionDAO.java
@@ -1,33 +1,64 @@
 package com.eborait.gsns.persistencia;
 
-import java.util.List;
+import java.sql.SQLException;
+import java.util.Collection;
 
-import com.eborait.gsns.dominio.entitymodel.RegionEnum;
+import com.eborait.gsns.dominio.entitymodel.EntregaVacunas;
 import com.eborait.gsns.dominio.entitymodel.Vacunacion;
 
-public class VacunacionDAO extends AbstractEntityDAO {
-
+/**
+ * Realiza la gestión de Vacunacion con la base de datos.
+ * 
+ * @author Jorge Fernández Escolano
+ * @author Roberto Esteban Olivares
+ * @since 1.0
+ * @version 1.0
+ * 
+ * @see Vacunacion
+ *
+ */
+public class VacunacionDAO extends AbstractEntityDAO<Vacunacion> {
 	/**
-	 * 
-	 * @param vacunacion
+	 * Formato sentencia insert.
 	 */
-	public void insertarVacunacion(Vacunacion vacunacion) {
-		// TODO - implement VacunacionDAO.insertarVacunacion
-		throw new UnsupportedOperationException();
+	private static final String INSERT = "INSERT INTO vacunacion VALUES(%s, %s, %s, %s)";
+
+	@Override
+	public Vacunacion get(String id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	public List<Vacunacion> seleccionarVacunaciones() {
-		// TODO - implement VacunacionDAO.seleccionarVacunaciones
-		throw new UnsupportedOperationException();
+	@Override
+	public Collection<Vacunacion> getAll(String criteria, String value) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	/**
+	 * Realiza una inserción en la base de datos.
 	 * 
-	 * @param region
+	 * @param vacunacion objeto Vacunacion a insertar.
+	 * @return El número de filas insertadas.
+	 * @throws SQLException Si se produce una excepción en la sentencia SQL.
+	 * @see Vacunacion
 	 */
-	public List<Vacunacion> seleccionarVacunaciones(RegionEnum region) {
-		// TODO - implement VacunacionDAO.seleccionarVacunaciones
-		throw new UnsupportedOperationException();
+	@Override
+	public int insert(Vacunacion vacunacion) throws SQLException {
+		return AgenteBD.getAgente().insert(String.format(INSERT, vacunacion.getVacuna().toString(),
+				vacunacion.getPaciente().toDatabase(), vacunacion.getFecha(), vacunacion.isSegundaDosis()));
+	}
+
+	@Override
+	public int update(Vacunacion vacunacion) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Vacunacion vacunacion) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
