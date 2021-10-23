d43e28ba0d121e145dfa3e1cca3f3b922c1aea26
escolanojorge <82335141+escolanojorge@users.noreply.github.com>
2021-10-23 14:42:07 +0200

    Se añade la documentación en EntregaDAO.

--- GSNS/src/main/java/com/eborait/gsns/persistencia/EntregaDAO.java
+++ GSNS/src/main/java/com/eborait/gsns/persistencia/EntregaDAO.java
@@ -8,12 +8,43 @@ import java.util.Vector;
 
 import com.eborait.gsns.dominio.entitymodel.EntregaVacunas;
 
+/**
+ * Realiza la gestión de EntregaVacunas con la base de datos.
+ * 
+ * @author Jorge Fernández Escolano
+ * @author Roberto Esteban Olivares
+ * @since 1.0
+ * @version 1.1
+ * 
+ * @see EntregaVacunas
+ *
+ */
 public class EntregaDAO extends AbstractEntityDAO<EntregaVacunas> {
+	/**
+	 * Formato sentencia select.
+	 */
 	private static final String SELECT = "SELECT FROM entrega_vacunas WHERE id = %s";
+	/**
+	 * Formato sentencia insert.
+	 */
 	private static final String INSERT = "INSERT INTO entrega_vacunas VALUES(%s, %s, %s, %d, %d, %s, %d)";
+	/**
+	 * Formato sentencia update.
+	 */
 	private static final String UPDATE = "UPDATE entrega_vacunas SET id = %s, lote = %s, fecha = %s, cantidad = %d, prioridad = %d, tipo_vacuna = %s, region = %d";
+	/**
+	 * Formato sentencia delete.
+	 */
 	private static final String DELETE = "DELETE FROM entrega_vacunas WHERE id = %s";
 
+	/**
+	 * Realiza una consulta a la base de datos.
+	 * 
+	 * @param id Identificador de EntregaVacunas que se busca.
+	 * @return Un objeto EntregaVacunas.
+	 * @throws SQLException Si se produce una excepción en la consulta SQL.
+	 * @see EntregaVacunas
+	 */
 	@Override
 	public EntregaVacunas get(String id) throws SQLException {
 		ResultSet rs = AgenteBD.getAgente().select(String.format(SELECT, id));
@@ -23,11 +54,28 @@ public class EntregaDAO extends AbstractEntityDAO<EntregaVacunas> {
 		return ev;
 	}
 
+	/**
+	 * Realiza una consulta a la base de datos.
+	 * 
+	 * @param criteria Columna por la que se filtra
+	 * @param value    Valor por el que se filtra.
+	 * @return Una colección con los objetos EntregaVacunas encontrados.
+	 * @throws SQLException Si se produce una excepción en la consulta SQL.
+	 * @see EntregaVacunas
+	 */
 	@Override
 	public Collection<EntregaVacunas> getAll(String criteria, String value) {
 		return null;
 	}
 
+	/**
+	 * Realiza una inserción en la base de datos.
+	 * 
+	 * @param entregaVacunas objeto EntregaVacunas a insertar.
+	 * @return El número de filas insertadas.
+	 * @throws SQLException Si se produce una excepción en la consulta SQL.
+	 * @see EntregaVacunas
+	 */
 	@Override
 	public int insert(EntregaVacunas entregaVacunas) throws SQLException {
 		return AgenteBD.getAgente()
@@ -37,11 +85,27 @@ public class EntregaDAO extends AbstractEntityDAO<EntregaVacunas> {
 						entregaVacunas.getRegion().getId()));
 	}
 
+	/**
+	 * Realiza una actualización en la base de datos.
+	 * 
+	 * @param entregaVacunas objeto EntregaVacunas a actualizar.
+	 * @return El número de filas actualizadas.
+	 * @throws SQLException Si se produce una excepción en la consulta SQL.
+	 * @see EntregaVacunas
+	 */
 	@Override
 	public int update(EntregaVacunas entregaVacunas) {
 		return -1;
 	}
 
+	/**
+	 * Realiza un borrado en la base de datos.
+	 * 
+	 * @param entregaVacunas objeto EntregaVacunas a eliminar.
+	 * @return El número de filas eliminadas.
+	 * @throws SQLException Si se produce una excepción en la consulta SQL.
+	 * @see EntregaVacunas
+	 */
 	@Override
 	public int delete(EntregaVacunas entregaVacunas) throws SQLException {
 		return AgenteBD.getAgente().delete(String.format(DELETE, entregaVacunas.getId()));
