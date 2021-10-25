ca58f0ab7838d29c8ede8a5947b75859dcb7faf8
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-25 23:17:16 +0200

    Añadimos clase y primer constructor

---
+++ GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaRegistrarVacunacion.java
@@ -0,0 +1,11 @@
+package com.eborait.gsns.presentacion;
+
+import javax.swing.JPanel;
+
+public class PantallaRegistrarVacunacion extends JPanel {
+
+	public PantallaRegistrarVacunacion(final Main frame) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
