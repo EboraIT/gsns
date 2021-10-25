53aa881ae133cf7e6dd72bbf1e00a270c3f1566c
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-25 23:19:53 +0200

    Se añade JPanel y titulo

--- GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaRegistrarVacunacion.java
+++ GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaRegistrarVacunacion.java
@@ -1,11 +1,42 @@
 package com.eborait.gsns.presentacion;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class PantallaRegistrarVacunacion extends JPanel {
 
 	public PantallaRegistrarVacunacion(final Main frame) {
-		// TODO Auto-generated constructor stub
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(new BorderLayout(0, 0));
+
+		JPanel topPanel = new JPanel();
+		add(topPanel, BorderLayout.NORTH);
+		topPanel.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
+
+		JPanel midPanel = new JPanel();
+		add(midPanel, BorderLayout.CENTER);
+
+		JButton btnVolver = new JButton("Volver al menú principal");
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.cambiarPanel(frame.getPanelMain());
+			}
+		});
+		topPanel.add(btnVolver);
+
+		JLabel lblTitulo = new JLabel("Gestión Sistema Regional de Salud/Registro de Vacunacion");
+		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		topPanel.add(lblTitulo);
+		midPanel.setLayout(null);
+		
 	}
 
 }
