6aadc5236b220b3fdf6312e818856a32effc8b0c
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-25 23:31:42 +0200

    Se crea clase JPanel para NuevoLote

---
+++ GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaAltaNuevoLote.java
@@ -0,0 +1,11 @@
+package com.eborait.gsns.presentacion;
+
+import javax.swing.JPanel;
+
+public class PantallaAltaNuevoLote extends JPanel {
+
+	public PantallaAltaNuevoLote(final Main frame) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
