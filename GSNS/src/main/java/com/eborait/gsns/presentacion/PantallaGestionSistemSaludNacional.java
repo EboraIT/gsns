9c198cbad7a1914c97259d52e4aba27172f92d64
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-25 23:08:05 +0200

    Se borra el main

--- GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaGestionSistemSaludNacional.java
+++ GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaGestionSistemSaludNacional.java
@@ -15,18 +15,7 @@ public class PantallaGestionSistemSaludNacional extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PantallaGestionSistemSaludNacional frame = new PantallaGestionSistemSaludNacional();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
 
 	/**
 	 * Create the frame.
