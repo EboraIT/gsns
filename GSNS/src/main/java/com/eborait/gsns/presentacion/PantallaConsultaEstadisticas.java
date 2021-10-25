0e55da7b688cfdb894f74c932f79385411e83208
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-25 23:07:57 +0200

    Se borra main

--- GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaConsultaEstadisticas.java
+++ GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaConsultaEstadisticas.java
@@ -16,18 +16,7 @@ public class PantallaConsultaEstadisticas extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PantallaConsultaEstadisticas frame = new PantallaConsultaEstadisticas();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
 
 	/**
 	 * Create the frame.
