214038bafbc5cf7d2942382cecbc4e911c120df6
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-25 23:32:38 +0200

    Se crea clase para JPanel PantallaCalculoEntrega

---
+++ GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaCalculoEntrega.java
@@ -0,0 +1,11 @@
+package com.eborait.gsns.presentacion;
+
+import javax.swing.JPanel;
+
+public class PantallaCalculoEntrega extends JPanel {
+
+	public PantallaCalculoEntrega(final Main frame) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
