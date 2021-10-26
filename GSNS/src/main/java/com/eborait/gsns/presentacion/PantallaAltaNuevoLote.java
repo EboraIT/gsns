4c76625353c113cb10272557646397a7604bc31d
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-10-26 19:28:47 +0200

    Se añade btnAtras y titulo

--- GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaAltaNuevoLote.java
+++ GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaAltaNuevoLote.java
@@ -1,11 +1,39 @@
 package com.eborait.gsns.presentacion;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class PantallaAltaNuevoLote extends JPanel {
+	private JTextField textField;
+	private JPanel contentPane;
 
 	public PantallaAltaNuevoLote(final Main frame) {
-		// TODO Auto-generated constructor stub
+		
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel topPanel = new JPanel();
+		add(topPanel, BorderLayout.NORTH);
+		topPanel.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
+		
+		JPanel midPanel = new JPanel();
+		add(midPanel, BorderLayout.CENTER);
+		
+		JLabel lblTitulo = new JLabel("Gestion Sistema Nacional de Salud/Alta nuevo lote");
+		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		topPanel.add(lblTitulo);
+		
+		JButton btnAtras = new JButton("Atras");
+		btnAtras.setBounds(563, 287, 89, 23);
+		topPanel.add(btnAtras);
+		
 	}
 
 }
