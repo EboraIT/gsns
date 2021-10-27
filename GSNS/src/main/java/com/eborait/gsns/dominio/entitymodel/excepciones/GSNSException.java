a124fc472bda8b5bb99b01046e3cf8e9a2d2d092
escolanojorge <82335141+escolanojorge@users.noreply.github.com>
2021-10-27 18:11:57 +0200

    GSNSException: optimización de código y documentación.

--- GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/excepciones/GSNSException.java
+++ GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/excepciones/GSNSException.java
@@ -1,13 +1,28 @@
 package com.eborait.gsns.dominio.entitymodel.excepciones;
 
+/**
+ * Excepción genérica de la aplicación.
+ * 
+ * @author Jorge Fernández Escolano
+ * @author Roberto Esteban Olivares
+ * @since 1.0
+ * @version 1.0
+ *
+ */
 public class GSNSException extends Exception {
 
-	public GSNSException() {
-		// TODO Auto-generated constructor stub
-	}
+	/**
+	 * serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
 
-	public GSNSException(String msg) {
-		super(msg);
+	/**
+	 * Construtor de la clase GSNSException.
+	 * 
+	 * @param mensaje Mensaje de la excepción.
+	 */
+	public GSNSException(String mensaje) {
+		super(mensaje);
 	}
 
 }
