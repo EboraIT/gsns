2881b54987bef1a0ffa289e2e6e8ee4c91c2a727
escolanojorge <82335141+escolanojorge@users.noreply.github.com>
2021-11-03 21:44:25 +0100

    Se añade documentación.

--- GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/EntregaVacunas.java
+++ GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/EntregaVacunas.java
@@ -4,18 +4,52 @@ import java.util.Date;
 
 import com.eborait.gsns.dominio.entitymodel.excepciones.GSNSException;
 
+/**
+ * Define la información relativa a una entrega de vacunas.
+ *
+ * @author Jorge Fernández Escolano
+ * @author Roberto Esteban Olivares
+ * @version 1.0
+ * @since 1.0
+ */
 public class EntregaVacunas {
 
+	/** El id. */
 	private String id;
+
+	/** El tipo de vacuna. */
 	private TipoVacuna tipo;
+
+	/** La region. */
 	private RegionEnum region;
+
+	/** El grupo de prioridad. */
 	private GrupoPrioridad grupoPrioridad;
+
+	/** El lote de vacunas. */
 	private LoteVacunas lote;
+
+	/** La fecha. */
 	private Date fecha;
+
+	/** La cantidad. */
 	private int cantidad;
 
-	public EntregaVacunas(String id, String lote, Date fecha, int cantidad, int prioridad, TipoVacuna tipo,
-			int region) throws GSNSException {
+	/**
+	 * Instancia un objeto EntregaVacunas.
+	 *
+	 * @param id        El id.
+	 * @param lote      El tipo de vacuna.
+	 * @param fecha     La fecha.
+	 * @param cantidad  La cantidad.
+	 * @param prioridad La prioridad.
+	 * @param tipo      El tipo de vacuna.
+	 * @param region    La región.
+	 * @throws GSNSException Si se produce una excepción en el grupo de prioridad o
+	 *                       la región.
+	 */
+	public EntregaVacunas(String id, String lote, Date fecha, int cantidad, int prioridad, TipoVacuna tipo, int region)
+			throws GSNSException {
 		this.id = id;
 		this.fecha = fecha;
 		this.lote = new LoteVacunas(lote, fecha, tipo, cantidad, tipo.getFarmaceutica());
@@ -29,6 +63,17 @@ public class EntregaVacunas {
 		this.lote.getEntregas().add(this);
 	}
 
+	/**
+	 * Instancia un objeto EntregaVacunas.
+	 *
+	 * @param id        El id.
+	 * @param lote      El tipo de vacuna.
+	 * @param fecha     La fecha.
+	 * @param cantidad  La cantidad.
+	 * @param prioridad La prioridad.
+	 * @param tipo      El tipo de vacuna.
+	 * @param region    La región.
+	 */
 	public EntregaVacunas(String id, String lote, Date fecha, int cantidad, int prioridad, String tipo, int region) {
 		this.id = id;
 		this.fecha = fecha;
@@ -48,58 +93,128 @@ public class EntregaVacunas {
 		this.lote.getEntregas().add(this);
 	}
 
+	/**
+	 * Se obtiene el id.
+	 *
+	 * @return El id.
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Establece el id.
+	 *
+	 * @param id El nuevo id.
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * Se obtiene el tipo de vacuna.
+	 *
+	 * @return El tipo de vacuna.
+	 */
 	public TipoVacuna getTipo() {
 		return tipo;
 	}
 
+	/**
+	 * Establece el tipo de vacuna.
+	 *
+	 * @param tipo El nuevo tipo de vacuna.
+	 */
 	public void setTipo(TipoVacuna tipo) {
 		this.tipo = tipo;
 	}
 
+	/**
+	 * Se obtiene la region.
+	 *
+	 * @return La region.
+	 */
 	public RegionEnum getRegion() {
 		return region;
 	}
 
+	/**
+	 * Establece la region.
+	 *
+	 * @param region La nueva region.
+	 */
 	public void setRegion(RegionEnum region) {
 		this.region = region;
 	}
 
+	/**
+	 * Se obtiene el grupo de prioridad.
+	 *
+	 * @return El grupo de prioridad.
+	 */
 	public GrupoPrioridad getGrupoPrioridad() {
 		return grupoPrioridad;
 	}
 
+	/**
+	 * Establece el grupo de prioridad.
+	 *
+	 * @param grupoPrioridad El nuevo grupo de prioridad.
+	 */
 	public void setGrupoPrioridad(GrupoPrioridad grupoPrioridad) {
 		this.grupoPrioridad = grupoPrioridad;
 	}
 
+	/**
+	 * Se obtiene el lote de vacunas.
+	 *
+	 * @return El lote de vacunas.
+	 */
 	public LoteVacunas getLote() {
 		return lote;
 	}
 
+	/**
+	 * Establece el lote de vacunas.
+	 *
+	 * @param lote El nuevo lote de vacunas.
+	 */
 	public void setLote(LoteVacunas lote) {
 		this.lote = lote;
 	}
 
+	/**
+	 * Se obtiene la fecha.
+	 *
+	 * @return La fecha.
+	 */
 	public Date getFecha() {
 		return fecha;
 	}
 
+	/**
+	 * Establece la fecha.
+	 *
+	 * @param fecha La nueva fecha.
+	 */
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
 
+	/**
+	 * Se obtiene la cantidad.
+	 *
+	 * @return La cantidad.
+	 */
 	public int getCantidad() {
 		return cantidad;
 	}
 
+	/**
+	 * Establece la cantidad.
+	 *
+	 * @param cantidad La nueva cantidad.
+	 */
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
--- GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/TipoVacuna.java
+++ GSNS/src/main/java/com/eborait/gsns/dominio/entitymodel/TipoVacuna.java
@@ -1,47 +1,107 @@
 package com.eborait.gsns.dominio.entitymodel;
 
+/**
+ * Define la información relativa a una entrega de vacunas.
+ *
+ * @author Jorge Fernández Escolano
+ * @author Roberto Esteban Olivares
+ * @version 1.0
+ * @since 1.0
+ */
 public class TipoVacuna {
 
+	/** El nombre. */
 	private String nombre;
+
+	/** La farmacéutica. */
 	private String farmaceutica;
+
+	/** La fecha de aprobación. */
 	private String fechaAprobacion;
 
+	/**
+	 * Instancia un nuevo tipo de vacuna.
+	 *
+	 * @param nombre          El nombre.
+	 * @param farmaceutica    La farmacéutica.
+	 * @param fechaAprobacion La fecha de aprobación.
+	 */
 	public TipoVacuna(String nombre, String farmaceutica, String fechaAprobacion) {
 		this.nombre = nombre;
 		this.farmaceutica = farmaceutica;
 		this.fechaAprobacion = fechaAprobacion;
 	}
 
+	/**
+	 * Instancia un nuevo tipo de vacuna.
+	 *
+	 * @param tipo El tipo de vacuna.
+	 */
 	public TipoVacuna(String tipo) {
 		this.nombre = tipo.substring(0, tipo.indexOf("-"));
 		this.farmaceutica = tipo.substring(tipo.indexOf("-") + 1, tipo.lastIndexOf("-"));
 		this.fechaAprobacion = tipo.substring(tipo.lastIndexOf("-") + 1);
 	}
 
+	/**
+	 * Se obtiene el nombre.
+	 *
+	 * @return El nombre.
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Establece el nombre.
+	 *
+	 * @param nombre El nuevo nombre.
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Se obtiene la farmacéutica.
+	 *
+	 * @return La farmacéutica
+	 */
 	public String getFarmaceutica() {
 		return farmaceutica;
 	}
 
+	/**
+	 * Establece la farmacéutica.
+	 *
+	 * @param farmaceutica La nueva farmacéutica.
+	 */
 	public void setFarmaceutica(String farmaceutica) {
 		this.farmaceutica = farmaceutica;
 	}
 
+	/**
+	 * Se obtiene la fecha de aprobación.
+	 *
+	 * @return La fecha de aprobación
+	 */
 	public String getFechaAprobacion() {
 		return fechaAprobacion;
 	}
 
+	/**
+	 * Establece la fecha de aprobación.
+	 *
+	 * @param fechaAprobacion La nueva fecha de aprobación.
+	 */
 	public void setFechaAprobacion(String fechaAprobacion) {
 		this.fechaAprobacion = fechaAprobacion;
 	}
 
+	/**
+	 * Transforma la clase TipoVacuna a String.
+	 *
+	 * @return La clase TipoVacuna como String.
+	 */
 	@Override
 	public String toString() {
 		return nombre + "-" + farmaceutica + "-" + fechaAprobacion;
