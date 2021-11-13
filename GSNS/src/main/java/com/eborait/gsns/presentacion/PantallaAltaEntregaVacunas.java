d74fd5effc619165e31f7a9f1beeedc2182dd85b
escolanojorge <82335141+escolanojorge@users.noreply.github.com>
2021-11-13 18:40:48 +0100

    Se reduce duplicidad de código en las pantallas.

--- GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaAltaEntregaVacunas.java
+++ GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaAltaEntregaVacunas.java
@@ -1,7 +1,5 @@
 package com.eborait.gsns.presentacion;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,9 +8,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
 
 import com.eborait.gsns.dominio.entitymodel.excepciones.GSNSException;
 
@@ -24,7 +20,7 @@ import com.eborait.gsns.dominio.entitymodel.excepciones.GSNSException;
  * @version 1.0
  * @since 1.0
  */
-public class PantallaAltaEntregaVacunas extends JPanel {
+public class PantallaAltaEntregaVacunas extends PanelBase {
 
 	/** El serialVersionUID. */
 	private static final long serialVersionUID = 1L;
@@ -56,28 +52,7 @@ public class PantallaAltaEntregaVacunas extends JPanel {
 	 * @param frame JFrame de la aplicación.
 	 */
 	public PantallaAltaEntregaVacunas(final Main frame) {
-		setBorder(new EmptyBorder(5, 5, 5, 5));
-		setLayout(new BorderLayout(0, 0));
-
-		JPanel topPanel = new JPanel();
-		add(topPanel, BorderLayout.NORTH);
-		topPanel.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
-
-		JPanel midPanel = new JPanel();
-		add(midPanel, BorderLayout.CENTER);
-
-		JButton btnVolver = new JButton("Volver al menú principal");
-		btnVolver.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.cambiarPanel(frame.getPanelMain());
-			}
-		});
-		topPanel.add(btnVolver);
-
-		JLabel lblTitulo = new JLabel("Gestión Sistema Regional de Salud/Alta entrega de vacunas");
-		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		topPanel.add(lblTitulo);
-		midPanel.setLayout(null);
+		super(frame, "Gestión Sistema Regional de Salud/Alta entrega de vacunas");
 
 		JLabel lblIdEntrega = new JLabel("Identificador de la entrega:");
 		lblIdEntrega.setBounds(10, 36, 201, 14);
--- GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaRegistrarVacunacion.java
+++ GSNS/src/main/java/com/eborait/gsns/presentacion/PantallaRegistrarVacunacion.java
@@ -1,8 +1,5 @@
 package com.eborait.gsns.presentacion;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,9 +8,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
 
 import com.eborait.gsns.dominio.entitymodel.excepciones.GSNSException;
 
@@ -25,7 +20,7 @@ import com.eborait.gsns.dominio.entitymodel.excepciones.GSNSException;
  * @version 1.0
  * @since 1.0
  */
-public class PantallaRegistrarVacunacion extends JPanel {
+public class PantallaRegistrarVacunacion extends PanelBase {
 
 	/** El serialVersionUID. */
 	private static final long serialVersionUID = 1L;
@@ -60,28 +55,7 @@ public class PantallaRegistrarVacunacion extends JPanel {
 	 * @param frame JFrame de la aplicación.
 	 */
 	public PantallaRegistrarVacunacion(final Main frame) {
-		setBorder(new EmptyBorder(5, 5, 5, 5));
-		setLayout(new BorderLayout(0, 0));
-
-		JPanel topPanel = new JPanel();
-		add(topPanel, BorderLayout.NORTH);
-		topPanel.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
-
-		JPanel midPanel = new JPanel();
-		add(midPanel, BorderLayout.CENTER);
-
-		JButton btnVolver = new JButton("Volver al menú principal");
-		btnVolver.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.cambiarPanel(frame.getPanelMain());
-			}
-		});
-		topPanel.add(btnVolver);
-
-		JLabel lblTitulo = new JLabel("Gestión Sistema Regional de Salud/Registro de vacunación");
-		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		topPanel.add(lblTitulo);
-		midPanel.setLayout(null);
+		super(frame, "Gestión Sistema Regional de Salud/Registro de vacunación");
 
 		JLabel lblNombre = new JLabel("Nombre:");
 		lblNombre.setBounds(10, 36, 201, 14);
