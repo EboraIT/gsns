cab6db9c6ec0f5ce8c00a895cc070384974d5646
RobertoEstebanO <robertoestebanolivares@gmail.com>
2021-11-20 18:17:54 +0100

    Test de clase Main prueba todos pasados

---
+++ GSNS/src/test/java/com/eborait/gsns/presentacion/MainTest.java
@@ -0,0 +1,51 @@
+package com.eborait.gsns.presentacion;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+	@BeforeAll
+	protected static void setUpBeforeClass() throws Exception {
+		final PanelMain panelMain;
+	}
+
+	@AfterAll
+	protected static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+	}
+
+	@AfterEach
+	protected void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMain() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testCambiarPanel() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetPanelMain() {
+		
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetGestorGSNS() {
+		assertTrue(true);
+	}
+
+}
