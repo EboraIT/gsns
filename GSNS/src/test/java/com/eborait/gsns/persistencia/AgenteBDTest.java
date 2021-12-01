56645edd3bf8fbb56ece802ce6ecd8442b97c248
escolanojorge <82335141+escolanojorge@users.noreply.github.com>
2021-12-01 12:56:25 +0100

    Se desarrollo test de Util y se crea la clase de pruebas de AgenteBD.

---
+++ GSNS/src/test/java/com/eborait/gsns/persistencia/AgenteBDTest.java
@@ -0,0 +1,75 @@
+package com.eborait.gsns.persistencia;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AgenteBDTest {
+
+	@BeforeAll
+	protected static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	protected static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+	}
+
+	@AfterEach
+	protected void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testAgenteBD() {
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testGetAgente() {
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testConectarBD() {
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testDesconectarBD() {
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testSelect() {
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testInsert() {
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testUpdate() {
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public final void testDelete() {
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
